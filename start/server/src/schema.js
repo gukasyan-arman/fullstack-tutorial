32a8ad9880a6dbb0012f6c80c265977960e77efb
Arman Gukasyan <gukasian.a.a@gmail.com>
2020-05-01 15:56:21 +0300

    3.3

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -32,15 +32,30 @@ enum PatchSize {
 }
 
 type Query {
-  launches: [Launch]!
-  launch(id: ID!): Launch
-  me: User
-}
+          launches( # replace the current launches query with this one.
+            """
+            The number of results to show. Must be >= 1. Default = 20
+            """
+            pageSize: Int
+            """
+            If you add a cursor here, it will only return results _after_ this cursor
+            """
+            after: String
+          ): LaunchConnection!
+          launch(id: ID!): Launch
+          me: User
+      }
 
 type Mutation {
   bookTrips(launchIds: [ID]!): TripUpdateResponse!
   cancelTrip(launchId: ID!): TripUpdateResponse!
   login(email: String): String # login token
+}
+
+type LaunchConnection { # add this below the Query type as an additional type.
+  cursor: String!
+  hasMore: Boolean!
+  launches: [Launch]!
 }
 
 type TripUpdateResponse {
